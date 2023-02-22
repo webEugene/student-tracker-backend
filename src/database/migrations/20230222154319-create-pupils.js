d75f6c2e41495e7ca8c6d3b236fcae0b25cea626
Yevhenii Bilan (Valtech.com) <yevhenii.bilan@valtech.com>
2023-02-22 23:04:28 +0200

    changed globally students to pupils

---
+++ src/database/migrations/20230222154319-create-pupils.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     */
+    await queryInterface.createTable('pupils', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      mobilePhone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birthday: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      avatar_path: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      group_id: {
+        type: Sequelize.UUID,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: null,
+        allowNull: true,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: null,
+        allowNull: true,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     */
+    await queryInterface.dropTable('pupils');
+  },
+};
