fe59838ceb1e24cc6b8c475ef89279af8a4c83db
Yevhenii Bilan (Valtech.com) <yevhenii.bilan@valtech.com>
2024-02-04 01:55:13 +0200

    refactoring ralations

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
 import { setupSwagger } from './swagger';
-const ngrok = require('@ngrok/ngrok');
-
+// import { SessionBuilder } from '@ngrok/ngrok';
+// const ngrok = require('@ngrok/ngrok');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
@@ -12,9 +12,14 @@ async function bootstrap() {
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 
   // Get your endpoint online
-  ngrok
-    .connect({ addr: 3000, authtoken_from_env: true })
-    .then(listener => console.log(`Ingress established at: ${listener.url()}`));
+  // Setup ngrok ingress
+  // const session = await new SessionBuilder().authtokenFromEnv().connect();
+  // const listener = await session.httpEndpoint().listen();
+  // // new Logger('main').log(`Ingress established at ${listener.url()}`);
+  // listener.forward(`localhost:${process.env.PORT}`);
+  // Get your endpoint online
+  // ngrok.connect({ addr: 3000, authtoken_from_env: true })
+  //     .then(listener => console.log(`Ingress established at: ${listener.url()}`));
 }
 
 bootstrap();
